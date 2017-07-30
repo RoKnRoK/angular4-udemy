b94b7aa415de1d04873c645f8d8c50856aa63bc7
Roman Kulikov <roman.kulikov.ufa@gmail.com>
2017-07-30 15:06:40 +0300

    Add basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './src/app/header/header.component';
-import { ShoppingListSectionComponent } from './src/app/shopping-list-section/shopping-list-section.component';
-import { RecipesSectionComponent } from './src/app/recipes-section/recipes-section.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListSectionComponent } from './shopping-list-section/shopping-list-section.component';
+import { RecipesSectionComponent } from './recipes-section/recipes-section.component';
 import { RecipesListComponent } from './recipes-section/recipes-list/recipes-list.component';
 import { RecipesDetailComponent } from './recipes-section/recipes-detail/recipes-detail.component';
 import { RecipeItemComponent } from './recipes-section/recipes-list/recipe-item/recipe-item.component';
