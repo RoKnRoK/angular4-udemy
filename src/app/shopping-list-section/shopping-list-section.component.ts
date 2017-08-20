ee62ef92bfb15edc313052971bb87eb48ea38b36
Roman Kulikov <roman.kulikov.ufa@gmail.com>
2017-08-20 14:42:44 +0300

    Add ShoppingListService

--- src/app/shopping-list-section/shopping-list-section.component.ts
+++ src/app/shopping-list-section/shopping-list-section.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list-section',
   templateUrl: './shopping-list-section.component.html',
-  styleUrls: ['./shopping-list-section.component.css']
+  styleUrls: ['./shopping-list-section.component.css'],
+  providers: [ShoppingListService]
 })
 export class ShoppingListSectionComponent implements OnInit {
 
-  ingredients: Ingredient[] = [
+/*  ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
-  ];
-  constructor() { }
+  ];*/
+  ingredients: Ingredient[];
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
   }
 
   onIngredientAdded(ingredient:Ingredient) {
-    this.ingredients.push(ingredient);
+    this.shoppingListService.addIngredient(ingredient);
   }
 
 }
